7b3c578deadf288f7dd92976c80946ef8137d307
Suraj DM <surajdm123@gmail.com>
2021-11-03 01:23:53 -0400

    Fixes #25: Changed routing.py

--- src/routing.py
+++ src/routing.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
-from controller import (task_controller)
+from src.controller import (task_controller)
 
 
 routes = Blueprint('routes', __name__)
 
-routes.register_blueprint(task_controller.task, url_prefix='/tasks')
+routes.register_blueprint(task_controller.tasks, url_prefix='/tasks')
