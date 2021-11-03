b8da689d22bc1d47de4fd9d613390fbef44d4586
ShreyanshPrajapati <shreyansh.prajapati55@gmail.com>
2021-11-03 19:00:09 -0400

    SQL helper module

--- src/models/sql_helper.py
+++ src/models/sql_helper.py
@@ -1,8 +1,40 @@
-
-
+import pymysql
+import os
 class sql_helper:
     def __init__(self):
-        pass
+        self.connection_obj = None
 
+    def connect_database(self):
+        try:
+            self.connection_obj = pymysql.connect(
+                    host='simpli.cx7psuz6gzvw.us-east-1.rds.amazonaws.com',
+                    port = 3306,
+                    user = 'admin',
+                    password = '1q2w3e4r5t',
+                    db = "simpli",
+                    autocommit=True
+                    ) 
+        except:
+            pass
+            #Need to import error handling class
+
+    def disconnect_database(self):
+        try:
+            self.connection_obj.close()
+        except:
+            pass
+            #Need to import error handling class
+    
     def run_query(self, query):
-        pass
+        self.connect_database()
+        tempCursor = self.connection_obj.cursor()
+        tempCursor.execute(query)
+        output = tempCursor.fetchall() 
+        self.disconnect_database()   
+        return output
+
+if __name__=='__main__':
+    sql = sql_helper()
+    output =sql.run_query("show tables")
+    print(output)
+    print("Hello")
