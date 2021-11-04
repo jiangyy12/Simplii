dfc5e659612fe1a63075b15541107508cb9f1064
Himanshu Gupta <himanshu199586@gmail.com>
2021-11-03 22:16:12 -0400

    Category Controller and Model

--- src/controller/category_controller.py
+++ src/controller/category_controller.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, request
+from src.models.category_model import category_model
 
+category = Blueprint('category', __name__, url_prefix='/category')
+category = category_model()
+
+def get_category():
+    return category.get_category()
+
+@categorys.route('', methods=['POST'])
+def create_category():
+    data = request.form
+    category.create_category(data)
+    return 'category created succesfully!', 200
--- src/models/category_model.py
+++ src/models/category_model.py
@@ -1,2 +1,36 @@
 
+import pandas as pd
+from sql_helper import sql_helper
+
+con = sql_helper()
+class category_model:
+    def __init__(self):
+        pass
+
+    def create_category(self, data):
+        columns = ''
+        values = ''
+        for key, value in data.items():
+            columns += str(key)+', '
+            values += "'"+str(value)+"', "
+
+        query = "INSERT INTO  ("+columns[:-2]+" ) VALUES (" + values[:-2]+" );"
+        con.run_query(query)
+        return
+    
+    def get_category(self, data):
+        query = "SELECT Category_name FROM category;"
+        result = con.run_query(query)
+        result = pd.DataFrame(list(result))
+        return result.to_dict('records')
+
+    def update_category(self, data):
+        values = ''
+        for key, value in data.items():
+            values += str(key)+"= '"+str(value)+"', "
+        query = "UPDATE tasks SET "+values[:-2]+";"
+        con.run_query(query)
+        return
+
+
 
