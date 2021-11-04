7a5cdb3ff50ec13b5045836a770ab0540fa0520b
ShreyanshPrajapati <shreyansh.prajapati55@gmail.com>
2021-11-04 16:43:00 -0400

    Added comment in tables db test module

--- UnitTests/database_tables_test.py
+++ UnitTests/database_tables_test.py
@@ -2,18 +2,21 @@ import unittest
 from sql_helper import sql_helper
 
 class Database_Table_Test(unittest.TestCase):
+    #Check number of tables
     def test_number_of_tables(self):
         sqlObj = sql_helper()
         tables = sqlObj.run_query("Show tables")
         numOfTables = len(tables)
         self.assertEqual(numOfTables, 4)
 
+    #check if specific table exists or not
     def test_check_table_exists(self):
         sqlObj = sql_helper()
         tables = sqlObj.run_query("Show tables")
         stringTables = str(tables)
         self.assertEqual("Login_info" in stringTables, True)
 
+    #check if there are no other tables
     def test_check_table_doesnot_exists(self):
         sqlObj = sql_helper()
         tables = sqlObj.run_query("Show tables")
