3556f46df7c127b025036e5071a594cce2803789
ShreyanshPrajapati <shreyansh.prajapati55@gmail.com>
2021-11-03 23:26:14 -0400

    database_connection_test

---
+++ UnitTests/database_connection_test.py
@@ -0,0 +1,13 @@
+import unittest
+from sql_helper import sql_helper
+
+class Database_Connection_Test(unittest.TestCase):
+    def test_number_of_tables(self):
+        sqlObj = sql_helper()
+        tables = sqlObj.run_query("Show tables")
+        print(tables)
+        self.assertEqual(tables != None, True)
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed")
