a0f05edcb90acebe1affe59e9fb64193bb479e2c
Sneha Aradhey <aradheysv@gmail.com>
2021-11-04 21:50:36 -0400

    test cases for login feature

--- src/login/login.py
+++ src/login/login.py
@@ -1,19 +1,19 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, session
 from flask import render_template, redirect
 
-login = Blueprint('login', __name__, url_prefix='/')
+login = Blueprint('login', __name__)
 
 @login.route('/login', methods=['GET'])
 def loginMethod():
-    """This function renders the edit task page."""
+    """This function renders the login page."""
     return render_template("login.html")
 
 @login.route('/login', methods=['POST'])
 def loginPostMethod():
-    """This function renders the edit task page."""
+    """This function logs in users and redirects to home page."""
     return redirect("/")
 
 @login.route('/logout', methods=['GET'])
 def logoutMethod():
-    """This function renders the edit task page."""
+    """This function logsout of the application and redirects to login page again."""
     return redirect("/login")
