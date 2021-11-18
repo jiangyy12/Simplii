ab2e2dac8beff5914f8341054296bcbd610f6aba
tianyiccong <89321097+tianyiccong@users.noreply.github.com>
2021-11-17 21:21:12 -0500

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: tasks
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version 8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -86,6 +86,7 @@ CREATE TABLE `Tasks` (
   `Duedate` datetime DEFAULT NULL,
   `Hours` int DEFAULT NULL,
   `Category` int DEFAULT NULL,
+  `Description` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`TaskID`),
   UNIQUE KEY `TaskID_UNIQUE` (`TaskID`),
   KEY `UserID_idx` (`UserID`),
